f078a562b3e84b459e340150a6a0cb5986454ef8
Korik <gokaykorik@gmail.com>
2023-10-01 23:43:28 +0300

    Initial Commit

---
+++ CatchTheKennyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheKennyGame
+//
+//  Created by Gökay Korik on 1.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
